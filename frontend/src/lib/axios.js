eeaa8b4784b897413cd96152fce9f29d5450075d
Biswa8260 <bisworanjanmishra3@gmail.com>
2025-07-18 17:35:24 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://gapsap-gayn.onrender.com" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://gapsap-gayn.onrender.com" ;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
